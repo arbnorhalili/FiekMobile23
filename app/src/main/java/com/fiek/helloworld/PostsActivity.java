16f5fbe9c4e1d4928d46a64c85b4a24a82316cbf
Arbnor Halili <arbnor.halili@snapp.team>
2023-05-10 18:38:33 +0200

    Requesting and handling reponse from an API endpoint on the web

---
+++ app/src/main/java/com/fiek/helloworld/PostsActivity.java
@@ -0,0 +1,64 @@
+package com.fiek.helloworld;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class PostsActivity extends AppCompatActivity {
+
+    OkHttpClient client = new OkHttpClient();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+
+        Request request = new Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .build();
+
+        /*try (Response response = client.newCall(request).execute()) {
+            Log.i("ResponseString",response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Looper.prepare();
+                Toast.makeText(PostsActivity.this, "Failed to load Posts", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful())
+                {
+                    Gson gson = new Gson();
+                    //Type listType = new TypeToken<List<PostClass>>(){}.getType();
+                    //List<PostClass> responseObjects = (List<PostClass>)gson.fromJson(response.body().string(),
+                    //        listType);
+
+                    PostClass obj = gson.fromJson(response.body().string(), PostClass.class);
+                    Log.i("ResponseString", obj.getTitle());
+                }
+            }
+        });
+    }
+}
